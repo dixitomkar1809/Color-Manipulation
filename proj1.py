aeb5a29d39deee025880cf5faa7ebf0184542ec8
dixitomkar1809 <dixitomkar1809@gmail.com>
2019-03-05 22:50:35 -0600

    All three programs are over

--- proj1.py
+++ proj1.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import sys
+from collections import Counter
+from math import floor
 
 if(len(sys.argv) != 7) :
     print(sys.argv[0], ": takes 6 arguments. Not ", len(sys.argv)-1)
@@ -18,6 +20,8 @@ name_output = sys.argv[6]
 # Declaring Essentials
 maxL = float('-inf')
 minL = float('inf')
+maxY = float('-inf')
+minY = float('inf')
 Xw = 0.95
 Yw = 1.0
 Zw = 1.09
@@ -36,7 +40,7 @@ if(inputImage is None) :
     print(sys.argv[0], ": Failed to read image from: ", name_input)
     sys.exit()
 
-# cv2.imshow("input image: " + name_input, inputImage)
+cv2.imshow("input image: " + name_input, inputImage)
 
 rows, cols, bands = inputImage.shape # bands == 3
 W1 = round(w1*(cols-1))
@@ -45,9 +49,9 @@ W2 = round(w2*(cols-1))
 H2 = round(h2*(rows-1))
 
 # inputImage[0] = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
-# inputImage[1] = [[255, 0, 0],[255, 0, 0],[255, 0, 0],[255, 0, 0]]
+# inputImage[1] = [[0, 0, 255],[0, 0, 255],[0, 0, 255],[0, 0, 255]]
 # inputImage[2] = [[100, 100, 100], [100, 100, 100], [100, 100, 100], [100, 100, 100]]
-# inputImage[3] = [[0, 100, 100], [0, 100, 100], [0, 100, 100], [0, 100, 100]]
+# inputImage[3] = [[100, 100, 0], [100, 100, 0], [100, 100, 0], [100, 100, 0]]
 
 # The transformation should be based on the
 # historgram of the pixels in the W1,W2,H1,H2 range.
@@ -62,7 +66,7 @@ H2 = round(h2*(rows-1))
 #         tmp[i, j] = [gray, gray, gray]
 #
 # cv2.imshow('tmp', tmp)
-# cv2.imwrite("1"+name_output, tmp)
+# cv2.imwrite("gray_"+name_output, tmp)
 
 # end of example of going over window
 
@@ -85,6 +89,9 @@ def XYZToxyY(X, Y, Z):
     y = Y/(X+Y+Z)
     return x, y, Y
 
+def xyYToXYZ(x, y, Y):
+    return x*Y/y, Y, (1-x-y)*Y/y
+
 def XYZToLuv(X, Y, Z):
     t = Y/Yw
     if t > 0.008856:
@@ -106,10 +113,21 @@ def XYZToLuv(X, Y, Z):
     v = (13 * L) * (vPrime - vw)
     return L, u, v
 
-def linearScale(x, max_value, min_value):
+def linearScaleHundred(x, max_value, min_value):
     # print("In Linear Scaling")
     A = 0
     B = 100
+    a = min_value
+    b = max_value
+    # print(A, B, a, b)
+    x = (((x-a)*(B-A))/(b-a)) + A
+    # print("X value", x)
+    return x
+
+def linearScaleOne(x, max_value, min_value):
+    # print("In Linear Scaling")
+    A = 0
+    B = 1
     a = min_value
     b = max_value
     # print(A, B, a, b)
@@ -137,7 +155,6 @@ def LuvToXYZ(L, u, v):
         Z = (Y * (3 - (0.75 * uPrime) - (5 * vPrime))) / vPrime
     return X, Y, Z
 
-
 def XYZtoRGB(X, Y, Z):
     a1 = np.array([X, Y, Z])
     a2 = np.array([[3.240479, -1.53715, -0.498535], [-0.969256, 1.875991, 0.041556], [0.055648, -0.204043, 1.057311]])
@@ -159,85 +176,192 @@ def XYZtoRGB(X, Y, Z):
 
 # First Program
 outputImage = np.zeros([rows, cols, bands], dtype=float)
-finalOutputImage = np.zeros([rows, cols, bands], dtype=np.uint8)
+finalOutputProgramOneImage = np.zeros([rows, cols, bands], dtype=np.uint8)
 
 for i in range(0, rows) :
     for j in range(0, cols) :
+        if i in range(H1, H2+1) and j in range(W1, W2+1):
+            b, g, r = inputImage[i, j]
 
-        b, g, r = inputImage[i, j]
+            # print()
+            # print("RGB", r, g, b)
 
-        # print()
-        # print("RGB", r, g, b)
+            # Converting to Non Linear RGB
+            r, g, b = r/255, g/255, b/255
+            # print("Non Linear RGB ", r, g, b)
 
-        # Converting to Non Linear RGB
-        r, g, b = r/255, g/255, b/255
-        # print("Non Linear RGB ", r, g, b)
+            # Converting to Linear RGB
+            r, g, b = invgamma(r), invgamma(g), invgamma(b)
+            # print("Linear RGB ",r, g, b)
 
-        # Converting to Linear RGB
-        r, g, b = invgamma(r), invgamma(g), invgamma(b)
-        # print("Linear RGB ",r, g, b)
+            # Converting Color Image to XYZ
+            X, Y, Z = rgbToxyz(r, g, b)
+            # print("X Y Z ", X, Y, Z)
 
-        # Converting Color Image to XYZ
-        X, Y, Z = rgbToxyz(r, g, b)
-        # print("X Y Z ", X, Y, Z)
+            # Converting to Luv
+            L, u, v = XYZToLuv(X, Y, Z)
+            # print("L, u, v ", L, u, v)
 
-        # Converting to Luv
-        L, u, v = XYZToLuv(X, Y, Z)
-
-        # Rounding to 3 decimals
-        # L, u, v = round(L, 3), round(u, 3), round(v, 3)
-        # print("L, u, v ", L, u, v)
-
-        minL = min(L, minL)
-        maxL = max(L, maxL)
-        outputImage[i,j] = [L, u, v]
-
-# print()
-print("MinL, MaxL", minL, maxL)
+            minL = min(L, minL)
+            maxL = max(L, maxL)
+            outputImage[i,j] = [L, u, v]
+        else:
+            outputImage[i, j] = inputImage[i, j]
 
 # Getting Lprime then converting it to XYZ then To Linear RGB and then to Non Linear RGB
 for i in range(0, rows):
     for j in range(0, cols):
-        L, u, v = outputImage[i, j]
-        # print("Before scaling L u v ", L, u, v)
+        if i in range(H1, H2 + 1) and j in range(W1, W2 + 1):
+            L, u, v = outputImage[i, j]
+            # print("Before scaling L u v ", L, u, v)
 
-        # Scaling L to L Prime
-        L = linearScale(L, maxL, minL)
-        # print("L' u v ", L, u, v )
+            # Scaling L to L Prime
+            L = linearScaleHundred(L, maxL, minL)
+            # print("L' u v ", L, u, v )
 
-        # Convertingg Luv to XYZ
-        X, Y, Z = LuvToXYZ(L, u, v)
-        # print("X Y Z", X, Y, Z)
+            # Convertingg Luv to XYZ
+            X, Y, Z = LuvToXYZ(L, u, v)
+            # print("X Y Z", X, Y, Z)
 
-        # Converting XYZ to RGB
-        R, G, B = XYZtoRGB(X, Y, Z)
-        # print("R G B", R, G, B)
-        finalOutputImage[i, j] = [B, G, R]
+            # Converting XYZ to RGB
+            R, G, B = XYZtoRGB(X, Y, Z)
+            # print("R G B", R, G, B)
+            finalOutputProgramOneImage[i, j] = [B, G, R]
+        else:
+            finalOutputProgramOneImage[i, j] = outputImage[i, j]
 
-cv2.imshow("output:", finalOutputImage)
-cv2.imwrite("First_Program_"+name_output, finalOutputImage);
+cv2.imshow("Program 1 : ", finalOutputProgramOneImage)
+cv2.imwrite("Program 1_"+name_output, finalOutputProgramOneImage);
 
 # Second Program
-outputImage = np.zeros([rows, cols, bands], dtype=np.uint8)
+outputImage = np.zeros([rows, cols, bands], dtype=float)
+finalOutputProgramTwoImage = np.zeros([rows, cols, bands], dtype=np.uint8)
+
+list_of_L = []
 
 for i in range(0, rows) :
     for j in range(0, cols) :
-        b, g, r = inputImage[i, j]
-        outputImage[i,j] = [b, g, r]
-cv2.imshow("output:", outputImage)
-cv2.imwrite(name_output, outputImage);
+        if i in range(H1, H2+1) and j in range(W1, W2+1):
+            b, g, r = inputImage[i, j]
 
-"""
+            # print()
+            # print("RGB", r, g, b)
+
+            # Converting to Non Linear RGB
+            r, g, b = r/255, g/255, b/255
+            # print("Non Linear RGB ", r, g, b)
+
+            # Converting to Linear RGB
+            r, g, b = invgamma(r), invgamma(g), invgamma(b)
+            # print("Linear RGB ",r, g, b)
+
+            # Converting Color Image to XYZ
+            X, Y, Z = rgbToxyz(r, g, b)
+            # print("X Y Z ", X, Y, Z)
+
+            # Converting to Luv
+            L, u, v = XYZToLuv(X, Y, Z)
+            # print("L, u, v ", L, u, v)
+
+            list_of_L.append(floor(L))
+            outputImage[i,j] = [L, u, v]
+        else:
+            outputImage[i, j] = inputImage[i, j]
+
+h_of_L = Counter(list_of_L)
+sorted_list_of_L = sorted(list(Counter(list_of_L).keys()))
+f_of_L = {}
+counter = 0
+for L in sorted_list_of_L:
+    counter+=h_of_L[L]
+    f_of_L[L] = counter
+# print("h of L", h_of_L)
+# print("f of L", f_of_L)
+new_L = {}
+f_i_minus_one = 0
+number_of_pixels = (H2-H1) * (W2-W1)
+for L in sorted_list_of_L:
+    new_L[L] = floor(((f_i_minus_one + f_of_L[L])/2) * (101/number_of_pixels))
+    f_i_minus_one = f_of_L[L]
+# print("new L ", new_L)
+
+for i in range(0, rows):
+    for j in range(0, cols):
+        if i in range(H1, H2 + 1) and j in range(W1, W2 + 1):
+            L, u, v = outputImage[i, j]
+            L = new_L[floor(L)]
+
+
+            # Convertingg Luv to XYZ
+            X, Y, Z = LuvToXYZ(L, u, v)
+            # print("X Y Z", X, Y, Z)
+
+            # Converting XYZ to RGB
+            R, G, B = XYZtoRGB(X, Y, Z)
+            # print("R G B", R, G, B)
+            finalOutputProgramTwoImage[i, j] = [B, G, R]
+        else:
+            finalOutputProgramTwoImage[i, j] = outputImage[i, j]
+
+cv2.imshow("Program 2:", finalOutputProgramTwoImage)
+cv2.imwrite("Program 2_"+name_output, finalOutputProgramTwoImage);
+
 # Third Program
-outputImage = np.zeros([rows, cols, bands], dtype=np.uint8)
+outputImage = np.zeros([rows, cols, bands], dtype=float)
+finalOutputProgramThreeImage = np.zeros([rows, cols, bands], dtype=np.uint8)
 
 for i in range(0, rows) :
     for j in range(0, cols) :
-        b, g, r = inputImage[i, j]
-        outputImage[i,j] = [b, g, r]
-cv2.imshow("output:", outputImage)
-cv2.imwrite(name_output, outputImage);
-"""
+        if i in range(H1, H2+1) and j in range(W1, W2+1):
+            b, g, r = inputImage[i, j]
+
+            # print()
+            # print("RGB", r, g, b)
+
+            # Converting to Non Linear RGB
+            r, g, b = r/255, g/255, b/255
+            # print("Non Linear RGB ", r, g, b)
+
+            # Converting to Linear RGB
+            r, g, b = invgamma(r), invgamma(g), invgamma(b)
+            # print("Linear RGB ",r, g, b)
+
+            # Converting Color Image to XYZ
+            X, Y, Z = rgbToxyz(r, g, b)
+            # print("X Y Z ", X, Y, Z)
+
+            x, y, Y = XYZToxyY(X, Y, Z)
+
+            minY = min(Y, minY)
+            maxY = max(Y, maxY)
+            outputImage[i, j] = [x, y, Y]
+        else:
+            outputImage[i, j] = inputImage[i, j]
+
+for i in range(0, rows):
+    for j in range(0, cols):
+        if i in range(H1, H2 + 1) and j in range(W1, W2 + 1):
+            x, y, Y = outputImage[i, j]
+            # print("Before scaling L u v ", L, u, v)
+
+            # Scaling L to L Prime
+            Y = linearScaleOne(Y, maxY, minY)
+            # print("L' u v ", L, u, v )
+
+            # Convertingg Luv to XYZ
+            X, Y, Z = xyYToXYZ(x, y, Y)
+            # print("X Y Z", X, Y, Z)
+
+            # Converting XYZ to RGB
+            R, G, B = XYZtoRGB(X, Y, Z)
+            # print("R G B", R, G, B)
+            finalOutputProgramThreeImage[i, j] = [B, G, R]
+        else:
+            finalOutputProgramThreeImage[i, j] = outputImage[i, j]
+
+
+cv2.imshow("Program 3:", finalOutputProgramThreeImage)
+cv2.imwrite("Program 3_"+name_output, finalOutputProgramThreeImage);
 
 # wait for key to exit
 cv2.waitKey(0)
